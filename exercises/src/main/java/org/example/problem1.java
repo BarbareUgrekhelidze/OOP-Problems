d97ca940d762d1952c93c202549bcc516975b455
BarbareUgrekhelidze <barbareugrekhelidze32@gmail.com>
2026-02-24 12:18:46 +0400

    add some dummy text

--- exercises/src/main/java/org/example/problem1.java
+++ exercises/src/main/java/org/example/problem1.java
@@ -3,5 +3,6 @@ package org.example;
 public class problem1 {
     public static void main(String[] args){
         System.out.println("finished seminar 1");
+        System.out.println("dummy text");
     }
 }
